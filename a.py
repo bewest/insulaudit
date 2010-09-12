3bfc263eb338579cefb78b6710e7abd3d94c453e
Ben West <bewest@gmail.com>
2010-09-11 19:45:29 -0700

    most of day

--- a.py
+++ a.py
@@ -11,36 +11,26 @@ import binascii
 from binascii import b2a_hex as dehex
 from pprint import pprint, pformat
 
-logging.basicConfig( )
+logging.basicConfig( stream=sys.stdout )
 log = logging.getLogger( 'carelink' )
 log.setLevel( logging.DEBUG )
 log.info( 'hello world' )
 
-example = '\x01U\x00\x00\x02\x00\x00\x00\x05\x04\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+example = '\x01U\x00\x00\x02\x00\x00\x00\x05\x04\x00mLink II\x01\x10\x02\x00\x01\x01\x03\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
 
 """
-  \x01 U
-  \x00 \x00
-  \x02 \x00
-  \x00 \x00
-  \x05 \x04
-  \x00 \x00
-  \x00 \x00
-  \x00 \x00 \x00
-  \x00 \x00 \x00
-  \x00 \x00 \x00
-  \x00 \x00 \x00
-  \x00 \x00 \x00
-  \x00 \x00 \x00
-  \x00 \x00 \x00
-  \x00 \x00 \x00
-  \x00 \x00 \x00
-  \x00 \x00 \x00
-  \x00 \x00 \x00
-  \x00 \x00 \x00
-  \x00 \x00 \x00
-  \x00 \x00
-  \x00 \x00\x00 \x00 \x00 \x00 \x00 \x00 \x00
+
+
+'\x01U\x00\x00\x02\x00\x00\x00\x05\x04\x00mLink II\x01\x10\x02\x00\x01\x01\x03\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+
+
+\x01
+U
+\x00
+
+\x00
+\x02\x00\x00\x00\x05\x04\x00mLink II\x01\x10\x02\x00\x01\x01\x03\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00
+
 """
 
 ERROR_LOOKUP = [ "NO ERROR",
@@ -64,8 +54,8 @@ class Command( object ):
   """
   label       = 'usb ack'
   # Default timeout for a read.
-  timeout     = 0
-  sleep       = 0
+  timeout     = 2
+  sleep       = 1
 
   min_reply_size  = 64
   def __init__( self, **kwds ):
@@ -87,8 +77,9 @@ class Command( object ):
                     agent =self.__class__.__name__,
                     label =self.label )
 
-  def __call__( self, device, reply ):
+  def __call__( self, reply, device=False ):
     self.last_reply = reply
+    reply.info = self
     return reply
 
 class USBStatus( Command ):
@@ -98,6 +89,19 @@ class USBStatus( Command ):
   ACK   = 85  # U
   NAK   = 102 # f
   label = 'usb.status'
+
+  def rfByteCount( self, count ):
+    ( x, y ) = count
+    return ( x & 0xFF ) << 8 | y & 0xFF;
+
+  def __call__( self, reply, *args ):
+    info = { 'commModuleStatus': reply.body[ 0 ]
+           , 'status'          : CarelinkComStatus( reply.body[ 2 ] )
+           , 'rfBytesAvailable': self.rfByteCount( reply.body[ 3:5 ] )
+           }
+    reply.info = info
+    log.debug( 'status reply: %r' % info )
+    return reply
    
 
 class USBProductInfo( Command ):
@@ -105,6 +109,21 @@ class USBProductInfo( Command ):
   code   = [ 4 ]
   SW_VER = 16
   label  = 'usb.productInfo'
+  rf_table = { 001: '868.35Mhz' ,
+               255: '916.5Mhz'  }
+
+  def __call__( self, reply, device ):
+    info = {
+      'rf.freq'          : self.rf_table.get( reply.body[ 5 ], 'UNKNOWN' )
+    , 'serial'           : reply.body[ 0:3 ]
+    , 'product.version'  : reply.body[ 3:5 ]
+    , 'description'      : str( reply.body[ 06:11 ] )
+    , 'software.version' : reply.body[ 16:18 ]
+    , 'interfaces'       : reply.body[ 18 ]
+    }
+    reply.info = info
+    log.info( 'usbproductinfo: %r' % info )
+    return reply
 
 
 class USBInterfaceStats( Command ):
@@ -113,10 +132,25 @@ class USBInterfaceStats( Command ):
   label         = 'usb.interfaceStats'
 
 class USBSignalStrength( Command ):
-  code  = [ 6 ]
+  code  = [ 6, 0 ]
   label = 'usb.signalStrength'
+  value = '??'
+
+  def __call__( self, reply, *args ):
+    self.reply = reply
+    self.value = reply.body[ 0 ]
+    log.info( '{0}: {1}dBm'.format( self.label, self.value ) )
+    return reply
+
+  def __repr__( self ):
+    return '<{agent}:code={code}, label={label} {signal}dBm>'\
+           .format( code   = repr( self.code ),
+                    agent  = self.__class__.__name__,
+                    signal = self.value,
+                    label  = self.label )
 
 class USBReadData( Command ):
+  # XXX: !!!!
   code  = [ 12 ]
   label = 'usb.readdata'
 
@@ -181,7 +215,7 @@ class CarelinkUsb( object ):
     log.debug( 'command {0} inspects {1}'.format(
                 repr( command ),
                 repr( reply ) ) )
-    reply    = command( self, reply )
+    reply    = command( reply, self )
     return reply
     
 
@@ -189,18 +223,77 @@ class CarelinkException( Exception ): pass
 
 class NoReplyException( CarelinkException ): pass
 
+class CarelinkComStatus( object ):
+  statmap = {
+      'receiving.complete'      : 0x01,
+      'receiving.progress'      : 0x02,
+      'transmit.progress'       : 0x04,
+      'interface.error'         : 0x08,
+      'error.receiving.overflow': 0x10,
+      'error.transmit.overflow' : 0x20
+  }
+  name  = 'ERROR'
+  value = '????'
+  def __init__( self, status ):
+    self.raw = status
+    self.name = ''.join( [ self.name, ' ',
+                         str( bytearray( [ status ] )[ 0 ] ) ] )
+    for k,v in self.statmap.iteritems( ):
+      log.debug( 'status lookup: {0}: {1}'.format( k,v ) )
+      if status & v > 0:
+        self.name  = k
+        self.value = status & v
+        break
+  def __str__( self ):
+    return self.name
+
+  def __repr__( self ):
+    return ''.join( [
+          '<', '{agent}',
+          ':', '{name}',
+          ':', 'raw={raw}',
+          ':', 'value={value}'
+             , '>' ] ).format(
+                raw   = self.raw,
+                name  = self.name,
+                value = self.value,
+                agent = self.__class__.__name__
+          )
+
+class ACK( object ):
+  ACK     = 85  # U
+  NAK     = 102 # f
+  __ack__ = 'ACK'
+  def __init__( self, head ):
+    ( self.power, self.error, self.code ) = head
+  
+  def __repr__( self ):
+    return (     self.power == 1
+             and self.error == self.ACK
+           ) and self.__ack__  or self.__nak__( )
+
+  def __nak__( self ):
+    return ''.join( [ chr( self.error )
+             , ' ', self.reason( )  ] )
+
+  def reason( self ):
+    return 'UNKNOWN REASON'
+    
 class Reply( object ):
+  
   log    = logging.getLogger( 'reply' )
-  status = 'UNINITIALIZED'
+  ack    = False
+
   def __init__( self, raw_reply ):
     self.log = logging.getLogger( self.__class__.__name__ )
     self.raw = raw_reply
     self.msg = bytearray( raw_reply )
     try:
-      self.read_status = self.msg[ 0 ]
-      self.status      = self.msg[ 2 ]
+      self.ack  = ACK( self.msg[ 0:3 ] )
+      self.body = self.msg[ 3: ]
     except IndexError, e:
       raise NoReplyException( e )
+    #self.readBytesAvailable = self.msg[ 3:4 ]
     self.printable = str( self.msg ).encode( 'string_escape' )
     log.debug( 'init reply.raw: %s' % self.printable )
 
@@ -213,48 +306,25 @@ class Reply( object ):
     return str( self.msg )
 
   def __repr__( self ):
-    return "<{agent}:status={status}:{0}>".format( self.printable,
+    return "<{agent}:ack={ack}:{0}>".format( self.printable,
                                    agent=self.__class__.__name__,
-                                   status=self.status )
+                                   ack=self.ack )
 
-  @classmethod
-  def toDict( klass, response ):
-    klass.log.debug( response )
-    D       = { 'raw': response }
-    S       = klass.dehex( response )
-    klass.log.debug( 'dehexed: %s' % S )
-    S.reverse( )
-    ack     = int( S.pop( ) )
-    success = response[ 1 ]
-    S.pop( )
-    S.reverse( )
-    split   = len( S ) - 3
-    body    = S[ :split    ]
-    tail    = S[  split-1: ]
-    D.update( 
-      { 'ack'    : ack
-      , 'body'   : body
-      , 'tail'   : tail
-      , 'success': success } )
-    klass.log.debug( 'response2dict: %s' % D )
-    return D
 
 
 if __name__ == '__main__':
   print 'hello world'
   
-
-
   port = '/dev/ttyUSB0'
   
   carelink = CarelinkUsb( port )
   
-  print carelink( USBStatus( ) )
-  #print carelink( USBProductInfo( ) )
-  #print carelink( USBSignalStrength( ) )
+  reply = carelink( USBStatus( ) )
+  print carelink( USBProductInfo( ) )
+  print carelink( USBSignalStrength( ) )
+  print carelink( USBInterfaceStats( ) )
   #print carelink( USBInterfaceStats( ) )
-  ##print carelink( USBInterfaceStats( ) )
-  #print carelink( USBReadData( timeout = 1 ) )
+  #print carelink( USBReadData( timeout = 2 ) )
 
 
 
