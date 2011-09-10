c1ea7807d6802cdada5680abfff64a9a571ded5d
Ben West <bewest@gmail.com>
2011-09-10 01:54:35 -0700

    MIT license

--- src/insulaudit/__init__.py
+++ src/insulaudit/__init__.py
@@ -6,7 +6,7 @@ created by a "modern," circa 2010, insulin therapy regimen.
 __author__    = "Ben West <bewest@gmail.com"
 __version__   = '0.0.1'
 __copyright__ = "Copyright 2010-2011, Ben West"
-__license__   = "BSD"
+__license__   = "MIT"
 __status__    = 'development'
 
 
