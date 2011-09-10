f7aba864d8091afab85d1ded25c371fd895e5a70
Ben West <bewest@gmail.com>
2011-09-09 22:40:49 -0700

    tweak setup script

--- setup.py
+++ setup.py
@@ -3,6 +3,13 @@ import sys
 from setuptools import setup, find_packages
 
 
+src = 'src'
+sys.path.append(src)
+import insulaudit
+version     = insulaudit.__version__
+author      = insulaudit.__author__
+description = insulaudit.__doc__
+license     = insulaudit.__license__
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
@@ -11,16 +18,16 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "insulaudit",
-    version = "0.0.1",
-    author = "Ben West",
+    name         = "insulaudit",
+    version      = version,
+    author       = author,
     author_email = "bewest@gmail.com",
-    description = ("Audit insulin therapy.  Increase fidelity of care."),
-    license = "BSD",
-    url = "http://github.com/bewest/insulaudit",
-    packages=find_packages('src'),
-    package_dir= { '': 'src' },
-    install_requires=[ 'pyserial', 'cli', 'numpy', 'dateutil' ],
+    description  = description,
+    license      = license,
+    url          = "http://github.com/bewest/insulaudit",
+    packages     = find_packages(src),
+    package_dir  = { '': src },
+    install_requires=[ 'pyserial', 'pyCLI', 'numpy', 'python-dateutil' ],
     long_description=read('README'),
     entry_points = {
       'console_scripts': [ 'insulaudit = insulaudit.main:main' ] },
--- src/insulaudit/__init__.py
+++ src/insulaudit/__init__.py
@@ -3,8 +3,11 @@ This python package provides a toolkit for dealing with data used and
 created by a "modern," circa 2010, insulin therapy regimen.
 
 """
-__author__  = "Ben West <bewest@gmail.com"
-__version__ = '0.01'
+__author__    = "Ben West <bewest@gmail.com"
+__version__   = '0.0.1'
+__copyright__ = "Copyright 2010-2011, Ben West"
+__license__   = "BSD"
+__status__    = 'development'
 
 
 import lib
