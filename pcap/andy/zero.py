2c26e540cf901ea48a6c115c8147e7d5c05c49c0
Ben West <bewest@gmail.com>
2012-06-15 01:10:05 -0700

    hexdumper

--- pcap/andy/zero.py
+++ pcap/andy/zero.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 
-import sys
-import argparse
+import sys, os
+from os.path import getsize
+import argparse, itertools
 
 from insulaudit import lib
 from insulaudit.log import logger
@@ -11,16 +12,56 @@ logger.setLevel(logging.DEBUG)
 
 from pprint import pprint, pformat
 
+CHUNK_SIZE = 64
+
 def get_opts():
   parser = argparse.ArgumentParser( )
   parser.add_argument(
     'input', nargs='+', help="Input files")
   return parser
 
+def get_raw_handle(pathish):
+  logger.info('opening %s' % (pathish))
+  handle = open(pathish)
+  return handle
+
+def hex_dump_data(data):
+  print lib.hexdump(bytearray(data))
+
+def read_chunk(handle):
+  logger.info('start reading: %s' % handle.tell( ))
+  return bytearray(handle.read(64))
+
+def do_chunk(handle):
+  chunk = read_chunk(handle)
+  hex_dump_data(chunk)
+
+def do_input(pathish):
+  handle = get_raw_handle(pathish)
+  pos  = handle.tell( )
+  size = getsize(pathish)
+  logger.info('opening %s (%s bytes)' % (pathish, size))
+
+  for i in itertools.count( ):
+    if pos < size:
+      logger.info('chunk: %s' % i)
+      do_chunk(handle)
+      pos = handle.tell( )
+    else:
+      break
+
+
+
+
 def main(*args):
   parser = get_opts( )
-  opts = parser.parse_args(*args)
-  logger.info('hello world %r %s' % (args, pformat(opts)))
+  opts = parser.parse_args( )
+  logger.info('opening %s' % (opts.input))
+
+  for item in opts.input:
+    do_input(item)
+
+
   
 
 
